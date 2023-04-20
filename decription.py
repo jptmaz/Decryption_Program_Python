5566218cbecda9ed45e43aea20da2cbf1ff6e6b7
JPTMAZ <jaspagmaz98@gmail.com>
2023-04-20 22:27:48 +0800

    This is the decryption program

---
+++ decription.py
@@ -0,0 +1,14 @@
+# Program was originally created on Thursday, April 06, 2023.
+
+print("Hello! What shall we decrypt today?")
+encrypted_text = input("Enter the message to be decrypted: ")
+
+# Decrypt it using the program
+
+decrypted_text = encrypted_text.replace("*", "a").replace("&","e").replace("#", "i").replace("+", "o").replace("!", "u")
+
+# Print the output
+
+print("Your encrypted text was " + encrypted_text)
+print("The decrypted message is " + decrypted_text)
+
